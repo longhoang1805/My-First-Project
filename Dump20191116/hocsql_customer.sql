71553fc66af30c132d73b5d92e2a9fd06a9bf1fc
hoanglong19it3 <hlong.19it3@sict.udn.vn>
2019-12-17 20:39:24 +0700

    SQL

---
+++ Dump20191116/hocsql_customer.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.17, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: hocsql
+-- ------------------------------------------------------
+-- Server version	8.0.17
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `customer`
+--
+
+LOCK TABLES `customer` WRITE;
+/*!40000 ALTER TABLE `customer` DISABLE KEYS */;
+INSERT INTO `customer` VALUES (4003561,'NGUYỄN ÂU DIỆU HIỀN','Nữ','0976194839','Công Nhân','Việt Nam','1992-03-16'),(4004162,'HUỲNH THỊ HỒNG ÁNH','Nam','0987654123','Giáo Viên','ViệtNam','1993-03-13'),(4004249,'Ha Seok Jin','Nữ','0913000336','Doanh Nhân','Hàn Quốc','1989-03-24'),(4004333,'LÊ THỊ HỒNG NHUNG','Nữ','0916984187','Công Nhân','Việt Nam','1996-03-18'),(4004391,'Senna','Nữ','0945226069','CEO','Mĩ','1996-05-02'),(4006212,'NGUYỄN NHẬT HOÀNG','Nam','0168557108','Diễn Viên','Việt Nam','1994-05-12'),(4006939,'NGUYỄN VĂN VIỆT','Nam','0944388756','Giáo Viên','Việt Nam','1996-03-03'),(4008125,'Lucian','Nam','0909247890','CEO','Anh','1996-12-05'),(4009703,'Ahn Suk Hwan','Nam','0683851024','Diễn Viên','Hàn Quốc','1995-03-06'),(4009948,'Adela','Nam','0383662875','Nhà Báo','Anh','1985-08-22'),(4010222,'Fiona','Nam','0908782610','Ca Sĩ','Mĩ','1986-11-11'),(16002952,'TRẦN MINH PHƯƠNG','Nữ','0982998031','Bác Sĩ','Việt Nam','1996-05-10'),(26018452,'Ceridwen','Nữ','0905950362 ','Diễn Viên','Mĩ','1996-11-29'),(28027380,'ĐÀO ĐỨC THIỆN','Nam','0167661640','Kế Toán','Việt Nam','1995-03-22'),(28029423,'PHẠM THỊ THÚY HOÀI','Nữ','0937316604 ','Công Nhân','Việt Nam','1987-08-18'),(30003326,'NGUYỄN ĐÌNH AN','Nam','0938475888','Công Nhân','Việt Nam','1995-12-23'),(30013375,'NGUYỄN VĂN TIẾN','Nam','097491213','Luật Sư','Việt Nam','1989-11-11'),(30013841,'Acacia','Nữ','0985015945','Diễn Viên','Anh','1990-05-19'),(30014727,'HOÀNG KHÁNH SƠN','Nam','0973628675','Công Nhân','Việt Nam','1995-11-18'),(31000055,'Goo Hye Sun','Nữ','0167661646','Ca Sĩ','Trung Quốc','1992-03-17'),(31006941,'HÀ THANH VINH','Nam','0933615379','Buôn Bán','Việt Nam','1995-12-29'),(32000045,'Choi Jeong-yoon','Nam','0975910549','Doanh Nhân','Trung Quốc','1988-08-16'),(32000852,'VÕ ĐỨC TRUNG','Nam','0973662207','Diễn Viên','Việt Nam','1987-11-10'),(32000855,'LÊ THỊ THU HẰNG','Nữ','0169349822','Doanh Nhân','Việt Nam','1995-03-22'),(32001552,'HUỲNH MAI PHƯƠNG','Nữ','0905970028','Buôn Bán','Việt Nam','1987-11-18'),(32002742,'NGÔ LÝ NGỌC TRÂM TUYÊN','Nữ','0933275525','Doanh Nhân','Việt Nam','1995-05-11'),(32003018,'Ahn Nae Sang','Nữ','0919038202','Doanh Nhân','Hàn Quốc','1980-12-15'),(32007585,'PHẠM DUY','Nam','0916060883','Doanh Nhân','Việt Nam','1995-03-14'),(33004190,'NGUYỄN THÀNH ĐẠT','Nam','0985354416 ','Streamer','Việt Nam','1978-08-13'),(34002762,'HUỲNH MINH THẮNG','Nam','0168659014','Công Nhân','Việt Nam','1995-05-11'),(34004640,'ĐOÀN MINH TRANG','Nữ','0987986959','Thư Kí','Việt Nam','1980-11-29'),(34005858,'HUỲNH QUANG TRUNG','Nam','0973154514','Nhà Báo','Việt Nam','1995-12-10'),(34009272,'ĐỖ TẤN TĨNH','Nam','0959747031','Ca Sĩ','Việt Nam','1968-08-22'),(34014541,'LÊ THỊ BÍCH NGỌC','Nữ','0973167407','Thư Kí','Việt Nam','1995-03-18'),(34344343,'NGUYỄN THÀNH LONG','Nam','0973545897','Kĩ Sư','Việt Nam','1967-11-10'),(35002763,'TRƯƠNG LÊ DUY','Nam','0906187925','Công Nhân','Việt Nam','1995-05-11'),(35004030,'Calliope','Nữ','0972231812','Diễn Viên','Mĩ','1994-11-09'),(35006148,'NGUYỄN VĂN LONG','Nam','0976967898','Doanh Nhân','Việt Nam','1995-03-08'),(35007910,'Kang Kyung Joon','Nam','0973213200 ','Ca Sĩ','Hàn Quốc','1992-10-10'),(35009119,'Gong Seung Yeon','Nam','0932909084',' Thư Kí','Đài Loan','1988-12-07'),(38001820,'ĐỒNG VĂN NAM','Nam','0971232366','Xây Dựng','Việt Nam','1995-11-06'),(38003208,'NGUYỄN DUY KHÁNH','Nam','0977736208 ','Diễn Viên','Việt Nam','1995-05-18'),(38006992,'HUỲNH THANH XUYẾN','Nữ','0972333435','Diễn Viên','Việt Nam','1995-08-10'),(38007000,'Bridget','Nam','0984365938','Diễn Viên','Mĩ','1988-03-29'),(38009759,'Jung Ho Bin','Nam','0383584129','Diễn Viên','Hàn Quốc','1988-11-22'),(40003987,'Javan','Nam','0905722274','Diễn Viên','Anh','1995-11-05'),(40005938,'Ramos','Nam','0914573405','Ca Sĩ','Anh','1988-02-12'),(40009429,'NGUYỄN QUÝ KHÁNH','Nam','0121238855','Doanh Nhân','Việt Nam','1995-05-04'),(40010904,'Clara Lee','Nữ','0168994930','Diễn Viên','Hàn Quốc','1988-11-18'),(40011354,'Bianca','Nữ','0985111262','Ca Sĩ','Mĩ','1995-03-11'),(40015627,'Jang Geun Suk','Nam','0915000336','Diễn Viên','Hàn Quốc','1988-08-12'),(40016786,'Yasuo','Nam','0902562278','Diễn Viên','Mĩ','1995-12-20'),(40020122,'HÀ THỊ KHUYÊN','Nữ','0168335503','Quản Lí','Việt Nam','1987-11-06'),(41006538,'NGUYỄN TRỌNG QUÝ MẠNH','Nam','0904262210','Diễn Viên','Việt Nam','1989-03-22'),(42000385,'NGÔ VĂN SANG','Nam','0166958444','Bác Sĩ','Việt Nam','1995-12-10'),(42005865,'Baek Sung Hyun','Nam','0793821363','Ca Sĩ','Hàn Quốc','1988-08-29'),(42006506,'TRƯƠNG QUANG NHÃ','Nam','0986940051','Diễn Viên','Việt Nam','1995-05-11');
+/*!40000 ALTER TABLE `customer` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-11-16 20:57:18
---
+++ Dump20191116/hocsql_room.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.17, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: hocsql
+-- ------------------------------------------------------
+-- Server version	8.0.17
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `room`
+--
+
+LOCK TABLES `room` WRITE;
+/*!40000 ALTER TABLE `room` DISABLE KEYS */;
+INSERT INTO `room` VALUES (1,'Standard','300.000'),(2,'Standard','300.000'),(3,'Standard','300.000'),(4,'Standard','300.000'),(5,'Standard','300.000'),(6,'Standard','300.000'),(7,'Standard','300.000'),(8,'Standard','300.000'),(9,'Standard','300.000'),(10,'Standard','300.000'),(11,'Standard','300.000'),(12,'Standard','300.000'),(13,'Standard','300.000'),(14,'Standard','300.000'),(15,'Standard','300.000'),(16,'Superior','450.000'),(17,'Superior','450.000'),(18,'Superior','450.000'),(19,'Superior','450.000'),(20,'Superior','450.000'),(21,'Superior','450.000'),(22,'Superior','450.000'),(23,'Superior','450.000'),(24,'Superior','450.000'),(25,'Superior','450.000'),(26,'Superior','450.000'),(27,'Superior','450.000'),(28,'Superior','450.000'),(29,'Superior','450.000'),(30,'Superior','450.000'),(31,'Deluxe','600.000'),(32,'Deluxe','600.000'),(33,'Deluxe','600.000'),(34,'Deluxe','600.000'),(35,'Deluxe','600.000'),(36,'Deluxe','600.000'),(37,'Deluxe','600.000'),(38,'Deluxe','600.000'),(39,'Deluxe','600.000'),(40,'Deluxe','600.000'),(41,'Deluxe','600.000'),(42,'Deluxe','600.000'),(43,'Deluxe','600.000'),(44,'Deluxe','600.000'),(45,'Deluxe','600.000'),(46,'Junior Suite','800.000'),(47,'Junior Suite','800.000'),(48,'Junior Suite','800.000'),(49,'Junior Suite','800.000'),(50,'Junior Suite','800.000'),(51,'Junior Suite','800.000'),(52,'Junior Suite','800.000'),(53,'Junior Suite','800.000'),(54,'Junior Suite','800.000'),(55,'Junior Suite','800.000'),(56,'Junior Suite','800.000'),(57,'Junior Suite','800.000'),(58,'Junior Suite','800.000'),(59,'Junior Suite','800.000'),(60,'Executive Suite','1.000.000'),(61,'Executive Suite','1.000.000'),(62,'Executive Suite','1.000.000'),(63,'Executive Suite','1.000.000'),(64,'Executive Suite','1.000.000'),(65,'Executive Suite','1.000.000'),(66,'Executive Suite','1.000.000'),(67,'Executive Suite','1.000.000'),(68,'Executive Suite','1.000.000'),(69,'Executive Suite','1.000.000'),(70,'Executive Suite','1.000.000'),(71,'Executive Suite','1.000.000'),(72,'Executive Suite','1.000.000'),(73,'Executive Suite','1.000.000'),(74,'Executive Suite','1.000.000'),(75,'Executive Suite','1.000.000');
+/*!40000 ALTER TABLE `room` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-11-16 20:57:19
